593734419d045fb9d294f06498af2e4c671b3e2f
Boris <boris.thibaut.boulanger@gmail.com>
2019-05-13 10:30:46 +0200

    refactoring 1 to 4 case

--- src/test/java/fr/NumberTest.java
+++ src/test/java/fr/NumberTest.java
@@ -1,13 +1,19 @@
 package fr;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
 public class NumberTest {
-    @Test
-    public void for_the_numbers_1_to_4_I_symbol_in_multiples_is_used() {
-        Number number = new Number(1);
 
-        Assertions.assertThat(number.numeral()).isEqualTo("I");
+    @Test
+    @Parameters({"1,I", "2,II", "3,III", "4,IIII"})
+    public void for_the_numbers_1_to_4_I_symbol_in_multiples_is_used(int numeric, String numeral) {
+        Number number = new Number(numeric);
+
+        Assertions.assertThat(number.numeral()).isEqualTo(numeral);
     }
 }
