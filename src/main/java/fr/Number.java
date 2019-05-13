5e4489735d3ad1ca5b84d626ce684e0701c00012
Boris <boris.thibaut.boulanger@gmail.com>
2019-05-13 12:16:45 +0200

    refactoring Numeral of 10,20,30 with X symbol multiples case wip

--- src/main/java/fr/Number.java
+++ src/main/java/fr/Number.java
@@ -1,9 +1,18 @@
 package fr;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.apache.commons.lang3.StringUtils.repeat;
 
 class Number {
     private int number;
+
+    private Map<Integer, String> romanSymbolMap = new HashMap<Integer, String>() {{
+        put(1, "I");
+        put(5, "V");
+        put(10, "X");
+    }};
 
     Number(int number) {
         this.number = number;
@@ -13,29 +22,28 @@ class Number {
         StringBuilder numeral = new StringBuilder();
         int remainder = this.number;
 
-        if (remainder / 10 > 0) {
-            int times = remainder / 10;
+        remainder = getRemainderAndUpdateNumeralForValue(numeral, remainder, 10);
 
-            numeral.append(repeat("X", times));
-            remainder -= 10 * times;
-        }
+        remainder = getRemainderAndUpdateNumeralForValue(numeral, remainder, 5);
 
-        if (remainder % 10 == 10 - 1) {
-            numeral.append("I" + "X");
-            remainder -= 10 - 1;
-        }
-
-        if (remainder / 5 > 0) {
-            numeral.append("V");
-            remainder -= 5;
-        }
-
-        if (remainder % 5 == 5 - 1) {
-            numeral.append("I" + "V");
-            remainder -= 5 - 1;
-        }
-        numeral.append(repeat("I", remainder));
+        getRemainderAndUpdateNumeralForValue(numeral, remainder, 1);
 
         return numeral.toString();
+    }
+
+    private int getRemainderAndUpdateNumeralForValue(StringBuilder numeral, int remainder, int value) {
+        int times = remainder / value;
+
+        if (times > 0) {
+            numeral.append(repeat(romanSymbolMap.get(value), times));
+            remainder -= value * times;
+        }
+
+        if (remainder != 0 && remainder % value == value - 1) {
+            numeral.append("I")
+                    .append(romanSymbolMap.get(value));
+            remainder -= value - 1;
+        }
+        return remainder;
     }
 }
