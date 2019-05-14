c208f1869c029e60138b875b85927984839a9aa9
Boris <boris.thibaut.boulanger@gmail.com>
2019-05-14 14:13:35 +0200

    refactoring factory

--- src/main/java/fr/NumeralFactory.java
+++ src/main/java/fr/NumeralFactory.java
@@ -1,71 +1,43 @@
 package fr;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.apache.commons.lang3.StringUtils.repeat;
-
 class NumeralFactory {
-    private Map<Integer, String> romanSymbolMap = new HashMap<Integer, String>() {{
-        put(1, "I");
-        put(5, "V");
-        put(10, "X");
-        put(50, "L");
-        put(100, "C");
-        put(500, "D");
-        put(1000, "M");
-    }};
-    private int remainder;
-    private final StringBuilder numeral = new StringBuilder();
 
-    NumeralFactory() {
-    }
+    String of(int value) {
+        int remainder = value;
+        StringBuilder roman = new StringBuilder();
 
-    NumeralFactory(Map<Integer, String> romanSymbolMap) {
-        this.romanSymbolMap = romanSymbolMap;
-    }
-
-    String of(int number) {
-        remainder = number;
-
-        buildNumeral();
-
-        return numeral.toString();
-    }
-
-    private void buildNumeral() {
-        int times = romanSymbolMap.size() > 3 ? romanSymbolMap.size() / 3 + 2 : romanSymbolMap.size();
-
-        for (int index = times; index > 0; index--) {
-            updateRemainderAndNumeralForValue(tenPower(index), tenPower(index - 1));
-            updateRemainderAndNumeralForValue(5 * tenPower(index - 1), tenPower(index - 1));
-        }
-        updateRemainderAndNumeralForValue(1, 1);
-    }
-
-    private int tenPower(int power) {
-        return (int) Math.pow(10, power);
-    }
-
-    private void updateRemainderAndNumeralForValue(int value, int previousValue) {
-        int times = remainder / value;
-
-        if (times > 0) {
-            updateNumeral(repeat(romanSymbolMap.get(value), times));
-            subtractRemainderBy(value * times);
+        for (ArabicToRoman number : ArabicToRoman.values()) {
+            while (remainder >= number.arabic) {
+                remainder -= number.arabic;
+                roman.append(number.roman);
+            }
         }
 
-        if (remainder != 0 && remainder % value >= value - previousValue) {
-            updateNumeral(romanSymbolMap.get(previousValue) + romanSymbolMap.get(value));
-            subtractRemainderBy(value - previousValue);
+        return roman.toString();
+    }
+
+    enum ArabicToRoman {
+
+        THOUSAND(1000, "M"),
+        NINE_HUNDRED(900, "CM"),
+        FIVE_HUNDRED(500, "D"),
+        FOUR_HUNDRED(400, "CD"),
+        HUNDRED(100, "C"),
+        NINETY(90, "XC"),
+        FIFTY(50, "L"),
+        FORTY(40, "XL"),
+        TEN(10, "X"),
+        NINE(9, "IX"),
+        FIVE(5, "V"),
+        FOUR(4, "IV"),
+        ONE(1, "I");
+
+        private final int arabic;
+        private final String roman;
+
+        ArabicToRoman(int arabic, String roman) {
+            this.arabic = arabic;
+            this.roman = roman;
         }
-    }
-
-    private void subtractRemainderBy(int value) {
-        remainder -= value;
-    }
-
-    private void updateNumeral(String string) {
-        this.numeral.append(string);
     }
 }
